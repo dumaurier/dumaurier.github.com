20d72dd1b283346ff4ff6d8ec233bd60765ca05f
dumuarier <joshvogt@gmail.com>
2017-02-10 00:17:15 -0500

    big changes. groundbreaking. revolutionary.
    Just kidding.

--- src/js/page/home.js
+++ src/js/page/home.js
@@ -1,1 +1,9 @@
-console.log('you can follow me on twitter @jshvgt');
+console.log('my twitter account is https://twitter.com/jshvgt');
+console.log('---------------');
+console.log('my public github account is https://github.com/dumaurier');
+console.log('---------------');
+console.log('my private github account is https://github.com/joshvogt');
+console.log('---------------');
+console.log('my codepen account is https://codepen.io/josh_vogt/');
+console.log('---------------');
+console.log('you can email me at joshvogt@gmail.com');
